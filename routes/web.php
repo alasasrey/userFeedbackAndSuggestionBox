9ca426568d260d779cb34ecb492f72dc04599191
ReyAlas-as <alasasrey5@gmail.com>
2025-06-18 09:38:38 +0800

    adding the auth middleware in web.php

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,11 @@ Route::post('/login', [AuthController::class, 'login']);
 
 Route::get('/signup', [AuthController::class, 'showSignup']);
 Route::post('/signup', [AuthController::class, 'register']);
+
+Route::middleware('auth')->group(function () {
+    Route::get('/home', function () {
+        return view('home');
+    });
+    Route::post('/feedback', [FeedbackController::class, 'store']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
