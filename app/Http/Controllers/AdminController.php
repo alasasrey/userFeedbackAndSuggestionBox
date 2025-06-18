9021e7504f703d9f17c5ff1a44d328e3c2f284cb
ReyAlas-as <alasasrey5@gmail.com>
2025-06-18 19:49:20 +0800

    adding the feedback method in  AdminController

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Feedback;
 
 class AdminController extends Controller
 {
-    //
+
+    public function feedback()
+    {
+        $feedbacks = Feedback::latest()->get();
+        return view('admin.feedback', compact('feedbacks'));
+    }
+
 }
