38a976ae104c87eb288d47cc085fc00d755656c8
ReyAlas-as <alasasrey5@gmail.com>
2025-06-18 09:37:02 +0800

    adding the signup get and post request in web.php

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,6 @@ Route::get('/', function () {
 
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
+
+Route::get('/signup', [AuthController::class, 'showSignup']);
+Route::post('/signup', [AuthController::class, 'register']);
