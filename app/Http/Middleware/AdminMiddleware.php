c692af95e7731ad1b62aa6f849dbf067b6a88231
ReyAlas-as <alasasrey5@gmail.com>
2025-06-18 19:34:29 +0800

    adding the handle method in  AdminMiddleware

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
 
 class AdminMiddleware
 {
@@ -13,8 +14,13 @@ class AdminMiddleware
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next): Response
+    public function handle($request, Closure $next)
     {
-        return $next($request);
+        if (Auth::check() && Auth::user()->is_admin) {
+            return $next($request);
+        }
+
+        abort(403, 'Unauthorized');
     }
+
 }
