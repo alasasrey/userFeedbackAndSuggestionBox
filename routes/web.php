f77cc5604bf5a5faf07bdaebc00cbb7faf110de5
ReyAlas-as <alasasrey5@gmail.com>
2025-06-18 09:35:59 +0800

    adding the login get and post request in web.php

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\FeedbackController;
 
+Route::get('/', function () {
+    return view('login');
+});
 
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
