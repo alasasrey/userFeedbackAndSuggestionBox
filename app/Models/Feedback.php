259061cf819f4bb15bbda656c6ae22fe1eacd214
ReyAlas-as <alasasrey5@gmail.com>
2025-06-18 10:55:45 +0800

    adding fillable to the feedback model

--- app/Models/Feedback.php
+++ app/Models/Feedback.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Feedback extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'name',
+        'email',
+        'service',
+        'rating',
+        'feedback',
+        'suggestions',
+    ];
 }
