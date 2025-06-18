e7edf12a9a64c1eb6c0942e90b9527626f032047
ReyAlas-as <alasasrey5@gmail.com>
2025-06-18 09:58:49 +0800

    adding the store method in FeedbackController

--- app/Http/Controllers/FeedbackController.php
+++ app/Http/Controllers/FeedbackController.php
@@ -3,8 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Feedback;
+use Illuminate\Support\Facades\Auth;
 
 class FeedbackController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $request->validate(['message' => 'required']);
+
+        Feedback::create([
+            'user_id' => Auth::id(),
+            'message' => $request->message,
+        ]);
+
+        return redirect()->back()->with('success', 'Feedback submitted!');
+    }
+
 }
