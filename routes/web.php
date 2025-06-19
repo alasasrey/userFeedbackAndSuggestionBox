82b01922a95b58782eeb34c05fe78df36f6e6cf3
ReyAlas-as <alasasrey5@gmail.com>
2025-06-19 21:18:42 +0800

    adding the edit, update, and delete for services, but for the suggestions added only delete for now

--- routes/web.php
+++ routes/web.php
@@ -35,8 +35,21 @@ Route::middleware([
     \Illuminate\Auth\Middleware\Authenticate::class,
     AdminMiddleware::class,
 ])->group(function () {
+    //Feedback
     Route::get('/admin/feedback', [AdminController::class, 'feedback'])->name('admin.feedback');
     Route::get('/admin/feedback/{id}/edit', [AdminController::class, 'editFeedback'])->name('admin.feedback.edit');
     Route::put('/admin/feedback/{id}', [AdminController::class, 'updateFeedback'])->name('admin.feedback.update');
     Route::delete('/admin/feedback/{id}', [AdminController::class, 'deleteFeedback'])->name('admin.feedback.delete');
+
+    
+    //Services
+    Route::get('/admin/services', [AdminController::class, 'services'])->name('admin.services');
+    Route::get('/admin/services/{id}/edit', [AdminController::class, 'editService'])->name('admin.services.edit');
+    Route::put('/admin/services/{id}', [AdminController::class, 'updateService'])->name('admin.services.update');
+    Route::delete('/admin/services/{id}', [AdminController::class, 'deleteService'])->name('admin.services.delete');
+    
+    //Suggestions delete only for now
+    Route::get('/admin/suggestions', [AdminController::class, 'suggestions'])->name('admin.suggestions');
+    Route::delete('/admin/suggestions/{id}', [AdminController::class, 'deleteSuggestion'])->name('admin.suggestions.delete');
+
 });
